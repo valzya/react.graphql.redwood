727d1cdcf856e1a71ad8328511fe76ae9353703a
Valerie Blackburn <57338688+valzya@users.noreply.github.com>
2020-12-18 11:11:21 -0600

    valzya-inventory-components-src-routes
    valzya-inventory-components-src-routes

--- Routes.js
+++ Routes.js
@@ -8,8 +8,15 @@ import Home from './views/Home'
 import Items from './views/Items'
 import Users from './views/Users'
 
+const Activities = React.lazy(() => import("./views/Activities"))
+const Alerts = React.lazy(() => import("./views/Alerts"))
+const Home = React.lazy(() => import("./views/Home"))
+const Items = React.lazy(() => import("./views/Items"))
+const Users = React.lazy(() => import("./views/Users"))
+
 function Routes() {
   return (
+  	<Suspense fallback=(<div>Loading...</div>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/items" component={Items} />
